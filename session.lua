d347902160249024983332ecb20dcccc7763c351
Johnny_niy😝 <ddmobilee332@gmail.com>
2026-05-28 17:17:57 +0700

    Add session.lua for shared session data management

---
+++ session.lua
@@ -0,0 +1,10 @@
+-- Ruenyai HUB v2.0 - Shared Session Data Bridge
+local Session = {
+    Username = nil,
+    Password = nil,
+    CurrentMap = nil,
+    MapID = nil,
+    IsOnline = false
+}
+
+return Session
